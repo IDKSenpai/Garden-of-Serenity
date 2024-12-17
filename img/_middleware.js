f59974309500a5785c8aef050a6f7c8e08c44592
Kech Kheang <k.kheang087@gmail.com>
2024-12-17 22:07:48 +0700

    Initial commit

--- img/_middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-  const url = req.nextUrl;
-  if (url.pathname.startsWith("/img")) {
-    return NextResponse.rewrite(new URL("/404", req.url)); // Redirect to 404
-  }
-  return NextResponse.next();
-}
